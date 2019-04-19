d74cfb67836f276585163b25d6df415d850308ba
Camille Chapleau <camille.chapleau@hotmail.fr>
2019-04-19 11:11:41 +0200

    passport strategy

---
+++ server/configs/passport.js
@@ -0,0 +1,43 @@
+//Passport is authentication middleware for Node
+const User = require("../models/User");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs"); // !!!
+const passport = require("passport");
+
+passport.serializeUser((loggedInUser, cb) => {
+  cb(null, loggedInUser._id);
+});
+
+passport.deserializeUser((userIdFromSession, cb) => {
+  User.findById(userIdFromSession, (err, userDocument) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+    cb(null, userDocument);
+  });
+});
+
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, next) => {
+    User.findOne({ email }, (err, foundUser) => {
+      console.log("PASSPORT STRATEGY", email, password, foundUser);
+      if (err) {
+        next(err);
+        return;
+      }
+
+      if (!foundUser) {
+        next(null, false, { message: "Incorrect username." });
+        return;
+      }
+
+      if (!bcrypt.compareSync(password, foundUser.password)) {
+        next(null, false, { message: "Incorrect password." });
+        return;
+      }
+
+      next(null, foundUser);
+    });
+  })
+);
